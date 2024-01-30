58a9d541796d7e2eb0c664619071f0ef7da6998c
Stephen Dawson <stephendawson@protonmail.com>
2024-01-30 22:33:16 +0000

    feat: add intial profile structure
    - add admin settings to admin.py
    - add initial profile page
    - add profile form
    - add initial view code
    - add url to urls.py on app and project
    - add profile link to navigation

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'default_phone_number',
+                    'default_street_address1', 'default_street_address2',
+                    'default_town_or_city', 'default_county',
+                    'default_postcode', 'default_country')
