ea3c501c0cf7154466e6794c4beff97b417accd5
Stephen Dawson <stephendawson@protonmail.com>
2024-02-01 17:14:04 +0000

    feat: add product detail page
    - add product detail template and content
    - add logic to views.py
    - add url path to urls.py
    - add link to page on product cards

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='all_products'),
+    path('', views.products, name='products'),
+    path('<int:id>/', views.product_detail, name='product_detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
-def all_products(request):
+def products(request):
     """ A view to show all products. Also 
     includes sorting and search queries.
     """
@@ -13,3 +13,16 @@ def all_products(request):
     }
 
     return render(request, 'product/products.html', context)
+
+
+def product_detail(request, id):
+    """ 
+    A view to show a specific product.
+    """
+    product = get_object_or_404(Product, pk=id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'product/product_detail.html', context)
