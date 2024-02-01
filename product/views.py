7c6ddbaf285f0ee1ed47e9845b73c90e1de0ddb3
Stephen Dawson <stephendawson@protonmail.com>
2024-02-01 22:22:34 +0000

    feat: add product add form and url

--- product/views.py
+++ product/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.db.models.functions import Lower
+from .forms import ProductForm
 
 
 def products(request):
@@ -53,7 +55,8 @@ def products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_sorting': sort,
+        'current_sorting': current_sorting,
+        'total_products_number': total_products_number,
     }
 
     return render(request, 'product/products.html', context)
@@ -67,7 +70,36 @@ def product_detail(request, id):
 
     context = {
         'product': product,
-        'current_sorting': current_sorting,
     }
 
     return render(request, 'product/product_detail.html', context)
+
+
+@login_required
+def add_product(request):
+    """ 
+    Add a product to the store 
+    """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can access that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request,
+                           ('Failed to add product. '
+                            'Please ensure the form is valid.'))
+    else:
+        form = ProductForm()
+
+    template = 'product/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
