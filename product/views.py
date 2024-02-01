a20c323f85cc3deb71ce19671ffcd6416d3a0aa5
Stephen Dawson <stephendawson@protonmail.com>
2024-02-01 17:58:28 +0000

    feat: add simple search to nav

--- product/views.py
+++ product/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
 def products(request):
-    """ A view to show all products. Also 
+    """ 
+    A view to show all products. Also 
     includes sorting and search queries.
     """
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter anything to search")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'product/products.html', context)
