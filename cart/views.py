53cfe228b26bf4689a400c6df2a824101c81d2be
Stephen Dawson <stephendawson@protonmail.com>
2024-02-03 18:13:14 +0000

    feat: add functionality to remove items from cart

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import (
-    render, redirect, get_object_or_404, reverse
+    render, redirect, get_object_or_404, reverse, HttpResponse
 )
 from django.contrib import messages
 from product.models import Product
@@ -7,7 +7,12 @@ from product.models import Product
 
 def view_cart(request):
     """ A view that renders the cart contents page """
-    return render(request, 'cart/cart.html')
+
+    context = {
+        'on_cart_page': True
+    }
+
+    return render(request, 'cart/cart.html', context)
 
 
 def add_to_cart(request, id):
@@ -33,7 +38,9 @@ def add_to_cart(request, id):
 
 
 def adjust_cart(request, item_id):
-    """Adjust the quantity of the specified product to the specified amount"""
+    """
+    Adjust the quantity of the specified product to the specified amount
+    """
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
@@ -52,3 +59,22 @@ def adjust_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+
+def remove_item_from_cart(request, item_id):
+    """
+    Removes the item from the shopping cart
+    """
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart')
+
+        request.session['cart'] = cart
+
+        return redirect(reverse('view_cart'))
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
