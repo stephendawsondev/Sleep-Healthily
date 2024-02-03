bceea21f1f423b69f4998d7d483557cdd7d2b1b1
Stephen Dawson <stephendawson@protonmail.com>
2024-02-03 19:53:48 +0000

    feat: update checkout view to access order form

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
 
 def view_checkout(request):
     """ A view that renders the checkout page """
 
-    return render(request, 'checkout/checkout.html')
+    cart = request.session.get('cart', {})
+    if not cart:
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
