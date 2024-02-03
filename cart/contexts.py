be196368a240a970b13b9a6948c0bcbbbca34201
Stephen Dawson <stephendawson@protonmail.com>
2024-02-03 14:58:58 +0000

    feat: update cart functionality
    - pass more data to the cart template
    - update cart template with new data
    - add quantity selector script to the
    product and cart pages
    - update base.html cart icon colour

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
+from django.shortcuts import get_object_or_404
 from decimal import Decimal
 from django.conf import settings
+from product.models import Product
 
 
 def cart_contents(request):
@@ -8,6 +10,20 @@ def cart_contents(request):
     subtotal = 0
     product_count = 0
     cart_total = 0
+    cart = request.session.get('cart', {})
+    free_shipping = False
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        subtotal += quantity * product.price
+        product_count += quantity
+        line_total = product.price * quantity
+        cart_items.append({
+            'id': item_id,
+            'quantity': quantity,
+            'line_total': line_total,
+            'product': product,
+        })
 
     if subtotal < settings.FREE_SHIPPING_THRESHOLD:
         shipping = subtotal * \
@@ -16,6 +32,7 @@ def cart_contents(request):
     else:
         shipping = 0
         free_shipping_remainder = 0
+        free_shipping = True
 
     cart_total = shipping + subtotal
 
@@ -27,6 +44,7 @@ def cart_contents(request):
         'shipping': shipping,
         'free_shipping_remainder': free_shipping_remainder,
         'free_shipping_threshold': settings.FREE_SHIPPING_THRESHOLD,
+        'free_shipping': free_shipping,
     }
 
     return context
