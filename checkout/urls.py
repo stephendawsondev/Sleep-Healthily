102f8bd97e1fecaf2c3d3bdc5ec249e5902e5356
Stephen Dawson <stephendawson@protonmail.com>
2024-02-04 19:51:32 +0000

    feat: add checkout cache to urls and view

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.view_checkout, name='view_checkout'),
+    path('order_summary/<order_number>',
+         views.order_summary,
+         name='order_summary'),
+    path('cache_checkout_data/',
+         views.cache_checkout_data,
+         name='cache_checkout_data'),
 
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -2,14 +2,37 @@ from django.shortcuts import (render, redirect, reverse,
                               get_object_or_404)
 from django.contrib import messages
 from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
 
 from product.models import Product
+from profiles.models import UserProfile
 from .models import Order, OrderLineItem
+
+from profiles.forms import UserProfileForm
 from .forms import OrderForm
 from cart.contexts import cart_contents
 
 import stripe
 import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, ('Sorry, your payment cannot be '
+                                 'processed right now. Please try '
+                                 'again later.'))
+        return HttpResponse(content=e, status=400)
 
 
 def view_checkout(request):
@@ -25,8 +48,8 @@ def view_checkout(request):
             'last_name': request.POST['last_name'],
             'email': request.POST['email'],
             'phone_number': request.POST['phone_number'],
-            'address_line1': request.POST['address_line1'],
-            'address_line2': request.POST['address_line2'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
             'town_or_city': request.POST['town_or_city'],
             'postcode': request.POST['postcode'],
             'country': request.POST['country'],
@@ -59,12 +82,15 @@ def view_checkout(request):
                     return redirect(reverse('cart'))
 
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            return redirect(
+                reverse('order_summary', args=[order.order_number])
+            )
         else:
             messages.error(request, "There was an error with the form. \
                            Please check your inputs again.")
 
     else:
+        cart = request.session.get('cart', {})
         if not cart:
             return redirect(reverse('products'))
         cart = request.session.get('cart', {})
@@ -95,3 +121,47 @@ def view_checkout(request):
         }
 
         return render(request, template, context)
+
+
+def order_summary(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+                'default_country': order.country,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Successfully completed order. \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'cart' in request.session:
+        del request.session['cart']
+
+    template = 'checkout/order_summary.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
