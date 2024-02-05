a054c6fa493467de4ecd2742ba0fa8227658a989
Stephen Dawson <stephendawson@protonmail.com>
2024-02-05 21:14:42 +0000

    feat: add 404 page to project

--- sleep_healthily/urls.py
+++ sleep_healthily/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,3 +13,5 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'boutique_ado.views.handler404
