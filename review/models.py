90bcfb54aaeaec7e8bbe7434e8a8a7f6a1bd8af6
Stephen Dawson <stephendawson@protonmail.com>
2024-02-06 16:14:25 +0000

    feat: update review model
    - add min and max length to review body
    - add user as foreign key
    - fix __str__ method to use title

--- review/models.py
+++ review/models.py
@@ -1,3 +1,5 @@
+from django.core.validators import MinLengthValidator
+from django.contrib.auth.models import User
 from django.db import models
 from product.models import Product
 
@@ -5,14 +7,20 @@ from product.models import Product
 class Review(models.Model):
     product = models.ForeignKey(
         Product, null=False, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, null=False, blank=False, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = models.TextField(
+        validators=[MinLengthValidator(
+            50, "Review must be greater than 50 characters")],
+        max_length=400
+    )
     rating = models.IntegerField()
     helpful_votes = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.title
 
     def helpful(self):
         self.helpful_votes += 1
