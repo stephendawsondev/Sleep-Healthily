dae485cca6e5141d1b61095a73d9befbc6de9685
Stephen Dawson <stephendawson@protonmail.com>
2024-02-07 21:07:47 +0000

    feat: pass a featured product to the homepage

--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from product.models import Product
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    """ A view to return the index page """
+
+    featured_product = Product.objects.all()[0]
+
+    context = {
+        'featured_product': featured_product,
+    }
+
+    return render(request, 'home/index.html', context)
