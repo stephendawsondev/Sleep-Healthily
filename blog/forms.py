91f2c9c1a9134d48c220cc953b4f4f1b4a86de4e
Stephen Dawson <stephendawson@protonmail.com>
2024-02-09 09:29:04 +0000

    feat: add custom form for blog post creation

--- blog/forms.py
+++ blog/forms.py
@@ -1,17 +1,28 @@
 from django import forms
 from product.widgets import CustomClearableFileInput
+
 from .models import BlogPost
+
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
 
 class BlogPostForm(forms.ModelForm):
+    """
+    The form for the blog post model.
+    """
 
     class Meta:
         model = BlogPost
-        fields = '__all__'
+        fields = ('status', 'title', 'content',
+                  'featured_image', 'tags', 'category')
+        widgets = {
+            'content': SummernoteWidget(),
+        }
 
-    image = forms.ImageField(label='Image',
-                             required=False,
-                             widget=CustomClearableFileInput)
+    featured_image = forms.ImageField(
+        label='Featured Image',
+        required=False,
+        widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
