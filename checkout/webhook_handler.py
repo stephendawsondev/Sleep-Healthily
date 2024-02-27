06802012f1d49839e2d0033ee50ec61322adcf33
Stephen Dawson <stephendawson@protonmail.com>
2024-02-27 16:47:37 +0000

    fix: pass in full name to webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -87,7 +87,8 @@ class StripeWH_Handler:
         while attempt <= 5:
             try:
                 order = Order.objects.get(
-                    full_name__iexact=shipping_details.name,
+                    full_name__iexact=f"{shipping_details.first_name} {
+                        shipping_details.last_name}",
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
@@ -115,7 +116,8 @@ class StripeWH_Handler:
             order = None
             try:
                 order = Order.objects.create(
-                    full_name=shipping_details.name,
+                    full_name=f"{shipping_details.first_name} {
+                        shipping_details.last_name}",
                     user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
